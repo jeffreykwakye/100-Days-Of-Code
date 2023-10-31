4fa08732c69a968df922017011e3d2bb748c2f92
Jeffrey Opare Kwakye <oparejeffrey@gmail.com>
2023-10-31 18:22:55 +0000

    Band_name_generator updated

---
+++ 1. Printing To Screen Etc/4. Band_name_generator.py
@@ -0,0 +1,11 @@
+# Band name generator for those of us who get stuck at naming stuff :)
+
+print("Hello and welcome to the Ultimate Band Name Generator!")
+print("With answers to just a few quick questions, we will have a Glorious name for you!")
+print("Let's get started shall we...\n")
+
+name_of_city = input("What is the name of the City you grew up in? \n")
+
+name_of_pet = input("What is the name of your pet or pet of a friend you like? \n")
+
+print(f"Your glorious band name is {name_of_city} {name_of_pet}")
