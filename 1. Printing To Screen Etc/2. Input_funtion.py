2a9f3a5be1f48bad3c24b5badcf286361e587d27
Jeffrey Opare Kwakye <oparejeffrey@gmail.com>
2023-10-31 18:08:15 +0000

    first commit

---
+++ 1. Printing To Screen Etc/2. Input_funtion.py
@@ -0,0 +1,5 @@
+# Input function is used to receive data from the user; 
+
+user_name = input("Please enter your name here and I will tell you how many characters are in it: ")
+
+print(f"There are {len(user_name)} characters in your name")
---
+++ 1. Printing To Screen Etc/3. Variables.py
@@ -0,0 +1,12 @@
+input_1 = input("a: ")
+input_2 = input("b: ")
+
+c = input_1 
+d = input_2   
+
+input_1 = d 
+input_2 = c 
+
+print("a and b will be switched around")
+print(f"a = {input_1}")
+print(f"b = {input_2}")
